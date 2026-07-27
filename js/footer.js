261cd1b693547331593ea6137d9d7479c531a274
“EDRIELLEN <egcambel@up.edu.ph>
2026-07-28 06:03:21 +0800

    fix: changed js for footer

---
+++ js/footer.js
@@ -0,0 +1,50 @@
+document.body.insertAdjacentHTML('beforeend', `
+    <footer class="site-footer">
+        <div class="footer-layout">
+
+            <div class="footer-left-group">
+                <h2 class="org-title">Samahang Sikolohiya</h2>
+
+                <div class="footer-info">
+                    <div class="info-column">
+                        <h3>Division</h3>
+                        <ul>
+                            <li>Psychology Cluster</li>
+                            <li>Division of Social Sciences</li>
+                            <li>College of Arts and Sciences</li>
+                            <li>University of the Philippines Visayas</li>
+                        </ul>
+                    </div>
+                    <div class="info-column">
+                        <h3>Contact</h3>
+                        <ul>
+                            <li>
+                                <span class="material-symbols-outlined">mail</span>
+                                <a href="mailto:samasik.upvisayas@gmail.com">samasik.upvisayas@gmail.com</a>
+                            </li>
+                            <li>
+                                <span class="material-symbols-outlined">photo_camera</span>
+                                <a href="#" target="_blank" rel="noopener">@upvsamasik</a>
+                            </li>
+                            <li>
+                                <span class="material-symbols-outlined">thumb_up</span>
+                                <a href="#" target="_blank" rel="noopener">Samahang Sikolohiya</a>
+                            </li>
+                            <li>
+                                <span class="material-symbols-outlined">smart_display</span>
+                                <a href="#" target="_blank" rel="noopener">Samahang Sikolohiya</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-logo">
+                <img src="/assets/images/logo_white.png" alt="Samahang Sikolohiya Logo" class="logo-img">
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2026 Samahang Sikolohiya. All rights reserved.</p>
+        </div>
+    </footer>
+    `);
